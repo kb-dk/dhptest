932c9558b09b4ed5fd498bc292476a0bcc3ff27e
Kåre Fiedler Christiansen <kfc@statsbiblioteket.dk>
2013-12-16 12:44:35 +0100

    Script for linking files for use by Quack

---
+++ linkfiles.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+TIFFHOME=/sbftp-home/scan-dk/DR
+MODSXML=MODS
+ALTO=ALTO
+
+if [ -z $2 ]; then
+  echo "Usage: $0 <batchdir> <outputdir>"
+  exit 1
+fi
+
+D=$1
+OUT=$2
+
+ls "$D/$ALTO/"*.xml | while read ALTOFILE; do
+  B=$(basename "$ALTOFILE" .xml)
+  MODSTIFFFIL="$(xpath -q -e "/mods:mods/mods:relatedItem/mods:identifier[@type='local']/text()" "$D/$MODSXML/$B-MODS.xml"|cut -d\" -f2).tif"
+  MODSTIFFDIR=$(echo "$MODSTIFFFIL"|cut -d_ -f1) 
+  TIFFFILE="$TIFFHOME/$MODSTIFFDIR/$MODSTIFFFIL"
+  ln -s "$ALTOFILE" "$OUT/$B.alto.xml"
+  ln -s "$TIFFFILE" "$OUT/$B.tif"
+done
