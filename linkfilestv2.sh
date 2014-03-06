5e4d246224571af05860d4e7a2b40e42ae8bbf90
Kåre Fiedler Christiansen <kfc@statsbiblioteket.dk>
2014-03-06 13:23:14 +0100

    Only generate quack for 100 files

--- linkfilestv2.sh
+++ linkfilestv2.sh
@@ -12,7 +12,7 @@ fi
 D=$1
 OUT=$2
 
-ls "$D/$ALTO/"*.xml | while read ALTOFILE; do
+ls "$D/$ALTO/"*.xml | sort -R | head -n 100 | while read ALTOFILE; do
   B=$(basename "$ALTOFILE" .xml)
   MODSTIFFFIL="$(xpath -q -e "/mods:mods/mods:relatedItem/mods:identifier[@type='local']/text()" "$D/$MODSXML/$B-MODS.xml"|cut -d\" -f2).tif"
   MODSTIFFDIR=$(echo "$MODSTIFFFIL"|cut -d_ -f1) 
